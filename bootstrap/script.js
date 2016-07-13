9bbeabd64d10233f5d1d258ea2a2887c5db89609
ISU Git Demo <isugitdemo@gmail.com>
2016-07-12 23:05:07 -0500

    Added card jquery example

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -6,3 +6,30 @@ $('#click-me').click(function() {
 	$(this).toggleClass('btn-success').toggleClass('btn-primary');
 
 });
+
+// Save's reference to Card image to variable
+var $cardImg = $('.card-img-top');
+// Save's reference to randomize button to variable
+var $randomBtn = $('#random');
+
+// Creates listener for image load event
+$cardImg.on('load', function() {
+
+	console.log('Image loaded'); // Output message to the console
+
+	$randomBtn.removeAttr('disabled'); // Enables button by removing 'disabled' attribute
+
+});
+
+// Creates listener for button click event
+$randomBtn.click(function() {
+
+	console.log('Getting new image'); // Output message to the console
+	
+	$randomBtn.attr('disabled','disabled'); // Disables button by adding 'disabled' attribute and setting is value to 'disabled'
+	
+	// Changes the Card image source
+	// Adds a timestamp to url to prevent caching
+	$cardImg.attr('src','https://placem.at/places?w=318&h=180&random=1&' + (new Date()).getTime()); 
+
+});
